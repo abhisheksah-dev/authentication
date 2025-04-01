8391e6c5bbdb0ee46c5b71142a037db137938860
AbhishekKumarSah99 <abhishekkumar220801@acropolis.in>
2025-04-01 15:57:29 +0530

    finally completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -230,8 +230,9 @@ export const resetPassword=async(req,res)=>{
       return res.status(400).json({success:false,message:"OTP expired"});
     }
 
-    const hashedPassword=await bcrypt.hash(newPassword,10);
-    user.newPassword=hashedPassword;
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+user.password = hashedPassword;
+
     user.resetOtp='';
     user.resetOtpExpireAt=0;
 
